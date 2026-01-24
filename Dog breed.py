cda592ab5e2b67fb666a78d8ecff7360742e57dd
Nabhya-yadav <nabhya.yadav20@gmail.com>
2026-01-24 11:11:25 +0530

    Add files via upload

---
+++ Dog breed.py
@@ -0,0 +1,9 @@
+class dog: 
+    def __init__ (self,color,breed):
+        self.color= color
+        self.breed=breed
+
+pitbull=dog("brown","American Pit Bull Terrier")
+
+print(pitbull.color)
+print(pitbull.breed)
